c92af6d0df74d762ab9052b055cf378049914058
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-11-20 08:52:34 +0100

    Main system

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test as")
+//const parser = require('rss-parser')
+/*** Discord, fs load ***/
+const Discord = require('discord.js')//, fs = require('fs')
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+/*** config and language load ***/
+let config = require('./config.json');
+//const lang = require('./languages/' + config.language + '.json');
+const system = {Discord, client, config};//, lang, fs};
+/*** Console logs ***/
+console.log(client);
+//client.on("error", (e) => console.error(e))
+//client.on("warn", (e) => console.warn(e))
+//client.on("debug", (e) => console.info(e))
+client.login(config.token);
