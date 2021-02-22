ee681226fc3baf42566904e848f459c49880eda9
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-02-22 09:41:13 +0100

    .

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const Discord = require('discord.js'), client = new Discord.Client(), cooldowns = new Discord.Collection(), fs = require('fs'), fsp = require('fs').promises, path = require('path')
 client.commands = new Discord.Collection();
 client.commands= new Map();
-const { error } = require('console');
-const { measureMemory } = require('vm');
+//const { error } = require('console');
+//const { measureMemory } = require('vm');
 /*** config and language load ***/
 let config = require('./config.json');
 const lang = require('./languages/' + config.language + '.json');
 const system = {Discord, client, config, lang, fs};
 /*** Console logs ***/
-//console.log(client);
+console.log(client);
 //client.on("error", (e) => console.error(e))
 //client.on("warn", (e) => console.warn(e))
 //client.on("debug", (e) => console.info(e))
